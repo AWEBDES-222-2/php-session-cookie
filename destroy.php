f8c67b4ebca74602160075bd4c7df1d34b0061c2
Dominic Deloso <70906273+dldeloso@users.noreply.github.com>
2023-02-22 16:08:36 +0800

    Update destroy.php

--- destroy.php
+++ destroy.php
@@ -1,5 +1,5 @@
 <?php
-  session_start(); //Start a Session
+  session_start(); //Make sure to place this before any other code
 
   session_destroy(); //Destroying all Sessions
   setcookie('my_cookie','myvalue',time() - 1000000); //Destroying a Cookie
