0c3c2fa7c51460bdbbdc187e578d2eccc5549c55
Dominic Deloso <70906273+dldeloso@users.noreply.github.com>
2023-02-22 16:08:16 +0800

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,6 +1,6 @@
 <?php
-  session_start();
+  session_start(); //Make sure to place this before any other code
 
   $_SESSION['session_name'] = "myvalue"; //Setting a Session
-  setcookie('cookie_name','muyvalue',time() + 1000000); //Setting a Cookie 
+  setcookie('cookie_name','myvalue',time() + 1000000); //Setting a Cookie 
 ?>
